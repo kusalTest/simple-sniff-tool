6ac30ef8cedf0a19f3c8109751ad6cb6ac82514a
kusalTest <kusalkandanaarachch@gmail.com>
2017-08-30 12:13:22 +0530

    test pcap_lookupdev() method

--- mainTool.cpp
+++ mainTool.cpp
@@ -3,7 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-	char *dev = argv[1];
+	char *dev = argv[1]; /* The device to snif on */
+	char *dev, errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
+	pcap_t *handle;		/* Session handle */
+
+	//dev = pcap_lookupdev(errbuf);
+	if (dev == NULL)
+	{
+		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+		return(2);
+	}
 
 	printf("Device: %s\n", dev);
 	return(0);
